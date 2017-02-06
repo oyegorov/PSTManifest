6a0324fa46105b3d9f7bd0f755fd5491715eac26
Oleg Yegorov <yegorov.oleg@gmail.com>
2017-02-06 07:51:49 -0500

    Initial implementation

---
+++ www/js/manifestController.js
@@ -0,0 +1,58 @@
+﻿angular.module('starter')
+    .controller('ManifestController', function ($scope, $ionicLoading, manifestService) {
+        $scope.getTimeDifferenceInfo = function(date) {
+            if ($scope.requestedOn == null)
+                return 0;
+
+            var date1_ms = Date.parse($scope.requestedOn);
+            var date2_ms = Date.parse(date);
+
+            var difference_ms = Math.abs(date2_ms - date1_ms);
+            var past = date2_ms - date1_ms < 0;
+
+            difference_ms = difference_ms / 1000;
+            difference_ms = difference_ms / 60;
+            var minutes = Math.floor(difference_ms) % 60;
+            difference_ms = difference_ms / 60;
+            var hours = Math.floor(difference_ms) % 24;
+
+            if (minutes == 0 && hours == 0) {
+                return {
+                    text: 'Now',
+                    past: false
+                }
+            };
+            if (hours > 24) {
+                return {
+                    text: 'Not today',
+                    past: true
+                }
+            };
+
+            var text = !past ? '' : '-';
+            if (hours != 0)
+                text += hours + ' hours ';
+            if (minutes != 0)
+                text += minutes + ' mins ';
+            
+            return {
+                text: text,
+                past: past
+            };
+        }
+
+        $scope.refresh = function () {
+            $scope.requestedOn = new Date();
+
+            $ionicLoading.show({
+                template: '<p>Loading...</p><ion-spinner></ion-spinner>'
+            });
+
+            manifestService.getManifest().then(function(manifest) {
+                $scope.manifest = manifest;
+                $ionicLoading.hide();
+            });
+        }
+
+        $scope.refresh();
+    });
---
+++ www/js/manifestService.js
@@ -0,0 +1,55 @@
+﻿angular.module('starter')
+    .factory('manifestService', function ($http, $filter) {
+        var timeout = 8000;
+        var manifestUrl = 'http://parachuteschool.com/manifest.json';
+
+        return {
+            getManifest: function () {
+                return $http.get(manifestUrl, { timeout: timeout }).then(function (response) {
+                    var manifestData = response.data;
+                    
+                    if (manifestData == null)
+                        return null;
+
+                    var loads = null;
+                    var manifestRecords = manifestData.ManifestInfoRecords;
+                    if (manifestRecords != null) {
+                        loads = [];
+                        var sortedRecords = $filter('orderBy')(manifestRecords, ['-ScheduledTakeOffTime', 'DisplayOrder']);
+
+                        var currentLoad = null;
+                        for (var i = 0; i < sortedRecords.length; i++) {
+                            var currentRecord = sortedRecords[i];
+                            if (currentLoad == null || currentLoad.name != currentRecord.FromManifestNumber) {
+                                currentLoad = {
+                                    name: currentRecord.FromManifestNumber,
+                                    scheduledTakeOffTime: new Date(Date.parse(currentRecord.ScheduledTakeOffTime) + new Date().getTimezoneOffset() * 60 * 1000),
+                                    status: currentRecord.Status,
+                                    jumperCapacity: currentRecord.JumperCapacity,
+                                    currentJumpers: currentRecord.CurrentJumpers,
+                                    jumpers: []
+                                }
+
+                                loads.push(currentLoad);
+                            }
+
+                            var jumper = {
+                                name: currentRecord.ProspectName == null ? currentRecord.JumperName : "***",
+                                jumpType: currentRecord.JumpType
+                            }
+
+                            currentLoad.jumpers.push(jumper);
+                        }
+                    }
+                    
+                    return {
+                        snapshotDateTime: new Date(Date.parse(manifestData.SnapshotDateTime)),
+                        loads: loads
+                    };
+                }, function (error) {
+                    return null;
+                });
+            }
+        }
+    });
+
